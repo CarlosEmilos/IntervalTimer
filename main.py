8b3515c71dddddb36291b788c035425f253173fe
CarlosEmilos <cessorensen@gmail.com>
2024-08-30 16:32:57 +0200

    created box layout and .kv file

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button 
+from kivy.uix.boxlayout import BoxLayout
 
+
+class MainWidget(Widget):
+    pass
 
 class timerApp(App):
     pass
+
+class BoxLayoutExample(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = "vertical"
+        b1 = Button(text="A")
+        b2 = Button(text="B")
+        self.add_widget(b1)
+        self.add_widget(b2)
 
 timerApp().run()
